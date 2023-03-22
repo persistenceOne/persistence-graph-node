7e581cabee903de7048bbae93db3cac11b7cc76c
mkRuby009 <manasi@persistence.one>
2023-03-22 12:29:44 +0400

    Graph node setup update: testnet and mainnet both

--- graph-node/scripts/init-firehose-node.sh
+++ graph-node/scripts/init-firehose-node.sh
@@ -80,12 +80,31 @@ END
 
     # Restore snapshot if url is present
     if [ ! -z "$STATE_RESTORE_SNAPSHOT_URL" ]; then
-        echo "Downloading snapshot from $STATE_RESTORE_SNAPSHOT_URL"
-        wget -O $HOME_DIR/snapshot.tar.gz $STATE_RESTORE_SNAPSHOT_URL
+        echo "=> Downloading snapshot from $STATE_RESTORE_SNAPSHOT_URL"
+        FILENAME=$(basename $STATE_RESTORE_SNAPSHOT_URL)
+        curl $STATE_RESTORE_SNAPSHOT_URL -o $HOME_DIR/$FILENAME
+        #only for validators
+        #cp $HOME_DIR/data/priv_validator_state.json $HOME/priv_validator_state_backup.json
 
-        echo "Extracting snapshot"
-        tar -xvf $HOME_DIR/snapshot.tar.gz -C $HOME_DIR
-        rm -rf $HOME_DIR/snapshot.tar.gz
+        case "$FILENAME" in
+            *.tar.lz4)
+                if ! command -v lz4 &> /dev/null; then
+                    apk add lz4
+                fi
+                echo "=> Extracting snapshot..."
+                lz4 -c -d $HOME_DIR/$FILENAME | tar -x -C $HOME_DIR
+                rm -rf $HOME_DIR/$FILENAME
+                ;;
+
+            *.tar.gz)
+                echo "=> Extracting snapshot..."
+                tar -xvf $HOME_DIR/$FILENAME -C $HOME_DIR
+                rm -rf $HOME_DIR/$FILENAME
+                ;;
+        esac
+
+        #mv $HOME_DIR/priv_validator_state_backup.json $HOME/data/priv_validator_state.json
+        #rm $HOME_DIR/priv_validator_state_backup.json
     fi
 fi
 
